9a1f4b68378b2ea3ee8a0d52ca3a15acf9f60786
EARTH157 <earthzz.jirapat@outlook.com>
2026-01-29 19:29:44 +0700

    refactor: reorganize publisher and subscriber initialization in Main_Processor class

--- src/processor_unit/processor_unit/main_processor.py
+++ src/processor_unit/processor_unit/main_processor.py
@@ -7,11 +7,17 @@ class Main_Processor(Node):
     def __init__(self):
         super().__init__('Main_processor_node')
 
-    def send_angle(self, angle):
-        msg = Float32()
-        msg.data = float(angle)
-        self.target_pub.publish(msg)
-        self.get_logger().info(f"📤 Sent Command: Move to {angle:.2f} deg")
+        self.publisher1_ = self.create_publisher(Float32, 'joint1/set_target_angle', 10)
+        self.publisher2_ = self.create_publisher(Float32, 'joint2/set_target_angle', 10)
+        self.publisher3_ = self.create_publisher(Float32, 'joint3/set_target_angle', 10)
+        
+        self.publisher1_calibrate_ = self.create_publisher(Float32, 'joint1/calibrate', 10)
+        self.publisher2_calibrate_ = self.create_publisher(Float32, 'joint2/calibrate', 10)
+        self.publisher3_calibrate_ = self.create_publisher(Float32, 'joint3/calibrate', 10)
+
+        self.subangle1_ = self.create_subscription(Float32, 'joint1/angle', self.angle_callback, 10)
+        self.subangle2_ = self.create_subscription(Float32, 'joint2/angle', self.angle_callback, 10)
+        self.subangle3_ = self.create_subscription(Float32, 'joint3/angle', self.angle_callback, 10)
 
 def main(args=None):
     rclpy.init(args=args)
